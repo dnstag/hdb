05645750a11cb1e2c857c7387bd786e8364e591a
yseibert <yannick.seibert@gemtex.de>
2026-06-05 15:15:41 +0200

    Added message formatting

--- src/hdb/api/__init__.py
+++ src/hdb/api/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2026 Yannick Seibert
 # SPDX-License-Identifier: MIT
 
+from hdb.api.error import APIDataError
+from hdb.api.http import HttpClient, UrllibHttpClient
 from hdb.api.pota import PotaAPIClient
 
-__all__ = ["PotaAPIClient"]
+__all__ = ["APIDataError", "HttpClient", "UrllibHttpClient", "PotaAPIClient"]
---
+++ src/hdb/formatting/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2026 Yannick Seibert
+# SPDX-License-Identifier: MIT
+
+from hdb.formatting.messages import FormattedField, FormattedMessage, MessageKind
+
+__all__ = ["FormattedField", "FormattedMessage", "MessageKind"]
